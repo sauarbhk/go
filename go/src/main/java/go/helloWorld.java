4d7984af28e966a76be472817ae31d10456697fc
Training <Training@SPICITY707>
2018-10-23 18:41:58 +0530

    BTAT Assessment Commit

---
+++ go/src/main/java/go/helloWorld.java
@@ -0,0 +1,17 @@
+package go;
+
+public class helloWorld {
+
+	
+		public static void main(String[] args) {
+		String[] sa = {"tom ", "jerry "};
+		for (int x = 0; x < 3; x++) {
+		for (String s : sa) {
+		System.out.print(x + " " + s);
+		if (x == 1) {
+		break;
+		}
+		}
+		}
+		}
+		}
